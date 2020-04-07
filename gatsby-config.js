d1086714151c5a0614b2f32826de877f85023b2a
Brant Wladichuk <brantwladichuk@gmail.com>
2020-04-07 15:41:03 -0400

    Meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Hotfix Podcast by Brant and Jeremy',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A weekly podcast that explores the fascinating world of Streaming, eSports and Gaming.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
